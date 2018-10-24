2494ee338cfa27c136f3a8764f9ec44f555bbc27
CaptpBdcht <vincent_mot@hotmail.fr>
2018-10-24 21:38:29 +0200

    Added following tests with first emerging algorithm

--- typescript/src/sort-it-out/sort-it-out.spec.ts
+++ typescript/src/sort-it-out/sort-it-out.spec.ts
@@ -8,12 +8,51 @@ describe('SortingBalls Rack should', () => {
 
     let rack: Rack;
 
-    beforeEach(() => {
+    before(() => {
         rack = new Rack();
     });
 
     it('be empty after instantiation', () => {
         rack.balls.should.exist;
         rack.balls.should.be.empty;
+    });
+
+    it('contain the element added when an element is added', () => {
+        rack.balls.should.exist;
+
+        rack.add(42);
+
+        rack.balls.should.be.of.length(1);
+        rack.balls.should.eql([ 42 ]);
+    });
+
+
+    it('contain both element ordered when a 2nd element is added', () => {
+        rack.balls.should.exist;
+
+        rack.add(7);
+
+        rack.balls.should.be.of.length(2);
+        rack.balls.should.eql([ 7, 42 ]);
+    });
+
+    it('contain all elements ordered when a 3rd element is added', () => {
+        rack.balls.should.exist;
+
+        rack.add(24);
+
+        rack.balls.should.be.of.length(3);
+        rack.balls.should.eql([ 7, 24, 42 ]);
+    });
+
+    it('contain all elements ordered when added three more elements', () => {
+        rack.balls.should.exist;
+
+        rack.add(100);
+        rack.add(58);
+        rack.add(1);
+
+        rack.balls.should.be.of.length(6);
+        rack.balls.should.eql([ 1, 7, 24, 42, 58, 100 ]);
     });
 });
--- typescript/src/sort-it-out/sort-it-out.ts
+++ typescript/src/sort-it-out/sort-it-out.ts
@@ -1,3 +1,25 @@
 export class Rack {
-    balls: any = [];
+    balls: number[] = [];
+
+    add(value: number) {
+        const size = this.balls.length;
+
+        if (size === 0)
+            this.balls.push(value);
+        else if (value < this.balls[0])
+            this.balls = [ value, ...this.balls ];
+        else if (value > this.balls[size - 1])
+            this.balls.push(value);
+        else {
+            for (let i = 1; i < size; i++) {
+                if (value < this.balls[i]) {
+                    this.balls = [
+                        ...this.balls.slice(0, i),
+                        value,
+                        ...this.balls.slice(i, size),
+                    ]
+                }
+            }
+        }
+    }
 }
