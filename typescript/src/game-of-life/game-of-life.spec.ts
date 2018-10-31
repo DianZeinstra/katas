02b482a0057dd29b89fe0249e8f84280621cad9a
CaptpBdcht <vincent_mot@hotmail.fr>
2018-10-31 14:59:27 +0100

    Added tests for game_of_life, first refacto for .ts

--- typescript/src/game-of-life/game-of-life.spec.ts
+++ typescript/src/game-of-life/game-of-life.spec.ts
@@ -20,26 +20,100 @@ describe('Game of Life should', () => {
             [ '.', '.' ]
         ];
 
-        const actual = JSON.stringify(sut.nextStepFor(current));
-        const expected = JSON.stringify(next);
-
-        actual.should.equal(expected);
+        sut.nextStepFor(current).should.eql(next);
     });
 
-    it('compute next state on 2x2 grid', () => {
+    it('kill an alive cell with zero alive neighbor', () => {
         const current = [
-            [ '*', '*' ],
-            [ '.', '*' ]
-        ];
-
-        const next = [
-            [ '.', '*' ],
+            [ '*', '.' ],
             [ '.', '.' ]
         ];
 
-        const actual = JSON.stringify(sut.nextStepFor(current));
-        const expected = JSON.stringify(next);
+        const next = [
+            [ '.', '.' ],
+            [ '.', '.' ]
+        ];
 
-        actual.should.equal(expected);
+        sut.nextStepFor(current).should.eql(next);
+    });
+
+    it('kill an alive cell with one alive neighbors', () => {
+        const current = [
+            [ '*', '*' ],
+            [ '.', '.' ]
+        ];
+
+        const next = [
+            [ '.', '.' ],
+            [ '.', '.' ]
+        ];
+
+        sut.nextStepFor(current).should.eql(next);
+    });
+
+    it('kill an alive cell with four alive neighbors', () => {
+        const current = [
+            [ '.', '*', '.' ],
+            [ '*', '*', '*' ],
+            [ '.', '*', '.' ]
+        ];
+
+        const next = [
+            [ '.', '.', '.' ],
+            [ '.', '.', '.' ],
+            [ '.', '.', '.' ]
+        ];
+
+        sut.nextStepFor(current).should.eql(next);
+    });
+
+    it('keep an alive cell with two or three alive neighbors', () => {
+        const current = [
+            [ '.', '*', '.' ],
+            [ '*', '*', '.' ],
+            [ '*', '*', '.' ]
+        ];
+
+        const next = [
+            [ '.', '.', '.' ],
+            [ '*', '*', '.' ],
+            [ '*', '*', '.' ]
+        ];
+
+        sut.nextStepFor(current).should.eql(next);
+    });
+
+    it('revive a dead cell with exactly three alive neighbors', () => {
+        const current = [
+            [ '.', '*', '.' ],
+            [ '*', '.', '.' ],
+            [ '.', '*', '.' ]
+        ];
+
+        const next = [
+            [ '.', '.', '.' ],
+            [ '.', '*', '.' ],
+            [ '.', '.', '.' ]
+        ];
+
+        sut.nextStepFor(current).should.eql(next);
+    });
+
+    it('correctly compute next state following all rules', () => {
+        const current = [
+            [ '.', '.', '*', '.' ],
+            [ '.', '*', '.', '*' ],
+            [ '*', '*', '.', '*' ],
+            [ '.', '*', '*', '.' ]
+        ];
+
+        const next = [
+            [ '.', '.', '.', '.' ],
+            [ '.', '.', '*', '.' ],
+            [ '.', '*', '*', '.' ],
+            [ '.', '*', '.', '.' ]
+        ];
+
+        sut.nextStepFor(current).should.eql(next);
     });
 });
--- typescript/src/game-of-life/game-of-life.ts
+++ typescript/src/game-of-life/game-of-life.ts
@@ -1,31 +1,65 @@
+/**
+ * 1. Any live cell with fewer than two live neighbours dies,
+ *    as if caused by underpopulation.
+ *
+ * 2. Any live cell with more than three live neighbours dies,
+ *    as if by overcrowding.
+ *
+ * 3. Any live cell with two or three live neighbours lives on.
+ *
+ * 4. Any dead cell with exactly three live neighbours becomes
+ *    a live cell.
+ */
 export enum States {
     ALIVE = '*',
     DEAD = '.'
 }
-
-/**
- * 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
- * 2. Any live cell with more than three live neighbours dies, as if by overcrowding.
- * 3. Any live cell with two or three live neighbours lives on to the next generation.
- * 4. Any dead cell with exactly three live neighbours becomes a live cell.
- */
 
 export class GameOfLife {
 
     constructor() {}
 
     nextStepFor(currentGame: string[][]) {
-        for (let line = 0; line < currentGame.length; line++) {
-            for (let column = 0; column < currentGame.length; column++) {
-                const neighbors = this.getNeighbors(currentGame, line, column);
-                const aliveNeighbors = neighbors.filter(s => s === States.ALIVE).length;
-            }
-        }
+        const nextGame: string[][] = [];
 
-        return currentGame;
+        for (let line = 0; line < currentGame.length; line++)
+            nextGame.push(this.getNextStepForLine(currentGame, line))
+
+        return nextGame;
     }
 
-    getNeighbors(game: string[][], line: number, column: number): string[] {
-        return [];
+    private getNextStepForLine(game: string[][], line: number) {
+        const nextLine: string[] = [];
+
+        for (let column = 0; column < game[line].length; column++)
+            nextLine.push(this.getNextState(game, line, column));
+
+        return nextLine;
+    }
+
+    private getNextState(game: string[][], line: number, column: number): string {
+        const isAlive = game[line][column] === States.ALIVE;
+        const aliveNeighbors = this.getAliveNeighborsOf(game, line, column);
+
+        if ((isAlive && (aliveNeighbors === 2 || aliveNeighbors === 3))
+        || (!isAlive && aliveNeighbors === 3))
+            return States.ALIVE;
+
+        return States.DEAD;
+    }
+
+    private getAliveNeighborsOf(game: string[][], line: number, column: number): number {
+        let aliveNeighbors = 0;
+
+        if (line > 0 && game[line - 1][column] === States.ALIVE)
+            aliveNeighbors++;
+        if (line < game.length - 1 && game[line + 1][column] === States.ALIVE)
+            aliveNeighbors++;
+        if (column > 0 && game[line][column - 1] === States.ALIVE)
+            aliveNeighbors++;
+        if (column < game[line].length - 1 && game[line][column + 1] === States.ALIVE)
+            aliveNeighbors++;
+
+        return aliveNeighbors;
     }
 }
