7cbb6d5ccb1ef6ed91114a8b7c488d03b65e28f1
unknown <vincent.milano@soat.fr>
2018-11-28 17:21:34 +0100

    Removed noImplicitThis
    Implemented first version for cipher

--- typescript/src/nihilist-cipher/nihilist-cipher.spec.ts
+++ typescript/src/nihilist-cipher/nihilist-cipher.spec.ts
@@ -6,16 +6,25 @@ should();
 
 describe('Nihilist Cipher should', () => {
 
-    const KEYWORD = 'keyword';
+    let KEYWORD = '';
 
     it('be instantiable', () => {
         const nc = new NihilistCipher();
         nc.should.exist;
     });
 
-    it('be instantiable, with a keyword parameter for polybe square', () => {
+    it('be instantiable, with a keyword parameter for polybius square', () => {
         const nc = new NihilistCipher(KEYWORD);
-        nc.should.exist;
         nc.getKeyword().should.equal(KEYWORD);
+    });
+
+    describe('be able to cipher a string with a password and no keyword', () => {
+
+        it('HELLO WORLD and TEST should return 64307273756074787329', () => {
+            const nc = new NihilistCipher(KEYWORD);
+            nc.cipher('HELLO WORLD', 'TEST').should.equal('64307273756074787329');
+        });
+
+
     });
 });
--- typescript/src/nihilist-cipher/nihilist-cipher.ts
+++ typescript/src/nihilist-cipher/nihilist-cipher.ts
@@ -1,10 +1,96 @@
+type PolybiusSquare = string[][];
+
+class PolybiusSquareUtil {
+    private static ALPHABET = 'ABCDEFGHIJKMLNOPQRSTUVWXYZ0123456789';
+    private static SIZE = 6;
+
+    static make(keyword: string): string[][] {
+        const cipherAlphabet = PolybiusSquareUtil.makeCipherAlphabet(keyword);
+        return PolybiusSquareUtil.stringToPolybiusSquare(cipherAlphabet);
+    }
+
+    private static makeCipherAlphabet(keyword: string) {
+        let result = removeDuplicateLetters(keyword).toUpperCase();
+        for (const letter of this.ALPHABET) {
+            if (result.indexOf(letter) === -1) {
+                result += letter;
+            }
+        }
+        return result;
+
+        function removeDuplicateLetters(str: string): string {
+            return [ ...new Set([ ...str ]) ].join('');
+        }
+    }
+
+    private static stringToPolybiusSquare(cipherAlphabet: string): PolybiusSquare {
+        const result: string[][] = [];
+        for (let step = 0; step < this.SIZE; step++) {
+            result.push(
+                cipherAlphabet
+                    .substr(step * this.SIZE, this.SIZE)
+                    .split('')
+            );
+        }
+        return result;
+    }
+
+    static valueOf(letter: string, square: PolybiusSquare): number {
+        for (let i = 0; i < this.SIZE; i++)
+            for (let j = 0; j < this.SIZE; j++)
+                if (square[ i ][ j ] === letter)
+                    return ((i + 1) * 10) + (j + 1);
+
+        return 0; // should handle exception
+    }
+}
+
 export class NihilistCipher {
 
-    constructor(private keyword: string = '') {
+    private polybiusSquare: PolybiusSquare;
 
+    constructor(private keyword: string = '') {
     }
 
     getKeyword(): string {
         return this.keyword;
+    }
+
+    cipher(plain: string, password: string): string {
+        const polybiusPlain = this.plainToPolybius(plain);
+        const key = this.generateKey(password, polybiusPlain.length);
+
+        return add(polybiusPlain, key).join('');
+
+        function add(left: number[], right: number[]): number[] {
+            let result = [];
+            for (let i = 0; i < left.length; i++)
+                result.push((left[i] + right[i]) % 100);
+            return result;
+        }
+    }
+
+    private plainToPolybius(plain: string): number[] {
+        this.buildPolybiusSquare();
+
+        return plain
+            .split('')
+            .filter(l => l !== ' ')
+            .map(l => PolybiusSquareUtil.valueOf(l, this.polybiusSquare))
+            .filter(val => val !== 0);
+    }
+
+    private buildPolybiusSquare(): void {
+        this.polybiusSquare = PolybiusSquareUtil.make(this.keyword);
+    }
+
+    private generateKey(password: string, length: number): number[] {
+        const result: number[] = [];
+        let index = 0;
+        while (result.length !== length) {
+            result.push(PolybiusSquareUtil.valueOf(password[index++], this.polybiusSquare));
+            if (index === password.length) index = 0;
+        }
+        return result;
     }
 }
