21d882f2fd01fc48584ef61b863f9eb337c3c40d
Vincent Milano <vincent@kerala.vc>
2020-01-15 16:34:31 +0100

    First Iteration

--- typescript/src/rpg/character/character.spec.ts
+++ typescript/src/rpg/character/character.spec.ts
@@ -1,38 +1,77 @@
 import { should } from 'chai';
-import { Character, CharacterBuilder } from './character';
+
+import {
+  Character,
+  CannotAutoAttackError,
+  CannotHealDeadCharacterError,
+  TooMuchLifeError
+} from './character';
 
 should();
 
 describe('Character should', () => {
   let character: Character;
-  let other: Character;
 
   beforeEach(() => {
-    character = new CharacterBuilder().withName('').build();
-    other = new CharacterBuilder().withName('').build();
+    character = Character.new();
   });
 
-  it('not create with more than 100 life', () => {
-    try {
-      new CharacterBuilder().withHealth(101).withName('').build()
-    }
-    catch (e) {
-      e.should.exist;
-    }
+  it('have a name', () => {
+    character.name.should.exist;
   });
 
-  describe('when created', () => {
-    it('be alive', () => {
-      character.alive().should.be.true;
+  it('have health pool initialized at 100', () => {
+    character.health.should.equal(100);
+  });
+
+  it('not create with a bigger health pool', () => {
+    (function () {
+      Character.new(101)
+    })
+    .should.throw(TooMuchLifeError);
+  });
+
+  it('be alive at creation', () => {
+    character.alive.should.equal(true);
+  });
+
+  it('be dead when it reaches 0 health', () => {
+    Character.new(0).alive.should.equal(false);
+  });
+
+  describe('attack', () => {
+    it('deals one damage to an alive enemy', () => {
+      const aliveEnemy = Character.new();
+      character.attack(aliveEnemy);
+      aliveEnemy.health.should.equal(99);
     });
 
-    it('have 100 life', () => {
-      character.health.should.exist;
-      character.health.should.equal(Character.MAX_HEALTH);
+    it ('deals no damage to a dead enemy', () => {
+      const deadEnemy = Character.new(0);
+      character.attack(deadEnemy);
+      deadEnemy.health.should.equal(0);
     });
 
-    it('have a name', () => {
-      character.name.should.exist;
+    it('cannot damage himself', () => {
+      (function () {
+        character.attack(character);
+      })
+      .should.throw(CannotAutoAttackError);
+    });
+  });
+
+  describe('heal', () => {
+    it('returns 1 point to an ally health pool', () => {
+      const ally = Character.new(50);
+      character.heal(ally);
+      ally.health.should.equal(51);
+    });
+
+    it('doesn\'t work for a dead character', () => {
+      (function () {
+        character.heal(Character.new(0))
+      })
+      .should.throw(CannotHealDeadCharacterError);
     });
   });
 });
--- typescript/src/rpg/character/character.ts
+++ typescript/src/rpg/character/character.ts
@@ -1,60 +1,72 @@
-class HealthTooHighException extends Error {
-  constructor(m: string) {
-    super(m);
+export class TooMuchLifeError extends Error {
+  constructor(m?: string) {
+    super(m || `Life cannot go above ${Character.MAX_HEALTH}`);
   }
 }
 
-export class CharacterBuilder implements CharacterBuilder {
-  private health = Character.MAX_HEALTH;
-  private name: string;
-
-  withHealth(health: number) {
-    if (health > Character.MAX_HEALTH) {
-      throw new HealthTooHighException(`Health too high (max is ${Character.MAX_HEALTH})`);
-    }
-
-    this.health = health;
-    return this;
+export class CannotAutoAttackError extends Error {
+  constructor(m?: string) {
+    super(m || `Life cannot go above ${Character.MAX_HEALTH}`);
   }
+}
 
-  withName(name: string) {
-    this.name = name;
-    return this;
-  }
-
-  build(): Character {
-    return new Character(this);
-  }
-
-  getHealth(): number {
-    return this.health;
-  }
-
-  getName(): string {
-    return this.name;
+export class CannotHealDeadCharacterError extends Error {
+  constructor(m?: string) {
+    super(m || `Life cannot go above ${Character.MAX_HEALTH}`);
   }
 }
 
 export class Character {
   static readonly MAX_HEALTH = 100;
 
-  private readonly _health: number;
-  private readonly _name: string;
-
-  get health(): number {
-    return this._health;
-  }
+  private _name: string;
+  private _health: number;
 
   get name(): string {
     return this._name;
   }
 
-  constructor(builder: CharacterBuilder) {
-    this._health = builder.getHealth();
-    this._name = builder.getName();
+  get health(): number {
+    return this._health;
   }
 
-  alive(): boolean {
+  get alive(): boolean {
     return this._health > 0;
+  }
+
+  private constructor(health: number) {
+    this._name = '';
+    this._health = health;
+  }
+
+  static new(health = Character.MAX_HEALTH): Character {
+    if (health > Character.MAX_HEALTH) {
+      throw new TooMuchLifeError();
+    }
+    return new Character(health);
+  }
+
+  attack(enemy: Character): void {
+    if (enemy === this) {
+      throw new CannotAutoAttackError();
+    }
+    if (enemy.alive) {
+      enemy.damage(1);
+    }
+  }
+
+  heal(ally: Character): void {
+    if (!ally.alive) {
+      throw new CannotHealDeadCharacterError();
+    }
+    ally.restore(1);
+  }
+
+  damage(quantity: number): void {
+    this._health -= quantity;
+  }
+
+  restore(quantity: number): void {
+    this._health += quantity;
   }
 }
