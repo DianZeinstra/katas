0eca71ad1531ecec080af08ceaab1fa65d41d33a
Vincent Milano <vincent@kerala.vc>
2020-02-25 19:48:20 +0100

    Fizzbuzz

--- typescript/src/fizzbuzz/fizzbuzz.spec.ts
+++ typescript/src/fizzbuzz/fizzbuzz.spec.ts
@@ -43,14 +43,14 @@ describe('FizzBuzz should', () => {
       .should.throw('Divisors must be distinct');
     });
 
-    it('should throw with one', () => {
+    it('should throw with ones', () => {
       (function () {
         fizzbuzz.withDivisors(1, 2).range(3);
       })
       .should.throw('Divisors cannot be 1');
     });
 
-    it('should throw with negative divisor', () => {
+    it('should throw with negative divisors', () => {
       (function () {
         fizzbuzz.withDivisors(-2, 5).range(3);
       })
@@ -76,9 +76,11 @@ describe('FizzBuzz should', () => {
       );
     });
 
-    it('should work with 3 and 7', () => {
-      fizzbuzz.withDivisors(3, 7).range(15).should.equal(
-        '1 2 Fizz 4 5 Fizz Buzz 8 Fizz 10 11 Fizz 13 Buzz Fizz'
+    it('should work with 3 and 7 on a high range', () => {
+      fizzbuzz.withDivisors(3, 7).range(63).should.equal(
+        '1 2 Fizz 4 5 Fizz Buzz 8 Fizz 10 11 Fizz 13 Buzz Fizz 16 17 Fizz 19 20 FizzBuzz' +
+        '22 23 Fizz 25 26 Fizz Buzz 29 Fizz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz 40 41 FizzBuzz' +
+        '43 44 Fizz 46 47 Fizz Buzz 50 Fizz 52 53 Fizz 55 Buzz Fizz 58 59 Fizz 61 62 FizzBuzz'
       );
     });
   });
